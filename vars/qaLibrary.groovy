24cf39041ba4140b0a508838002d6289f2ea6cca
mickey23alejo <mickey23alejo@gmail.com>
2020-12-23 12:04:17 -0500

    prueba

--- vars/qaLibrary.groovy
+++ vars/qaLibrary.groovy
@@ -35,7 +35,7 @@ def call(body) {
         stages {
             stage('Docker build') {
                 steps {
-                  sh "buildah bud -f Dockerfile -t qa-'${config.name}'-image:v1.0.$BUILD_NUMBER . "
+                  sh "docker build -f Dockerfile -t qa-'${config.name}'-image:v1.0.$BUILD_NUMBER . "
                     // container('docker'){
                     //         sh "cd $WORKSPACE"
                     //         sh "docker images"
