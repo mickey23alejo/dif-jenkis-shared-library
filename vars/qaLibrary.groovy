d3cb0278898888496808b834ac4f16e0ac73d6e8
mickey23alejo <mickey23alejo@gmail.com>
2020-12-23 09:32:27 -0500

    pruebas

--- vars/qaLibrary.groovy
+++ vars/qaLibrary.groovy
@@ -21,7 +21,8 @@ def call(body) {
                 containers:
                 - name: docker
                   image: docker:dind
-                  command: ["dockerd"]
+                  command: 
+                  - cat
                   tty: true
                 - name: oc-client
                   image: widerin/openshift-cli
