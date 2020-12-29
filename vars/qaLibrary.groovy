7b74a78457d597c39db071b409fa34d3515b5ede
mickey23alejo <mickey23alejo@gmail.com>
2020-12-28 21:20:01 -0500

    cambios

--- vars/qaLibrary.groovy
+++ vars/qaLibrary.groovy
@@ -30,7 +30,7 @@ def call(body) {
                   env: 
                     - name: DOCKER_HOST 
                       value: tcp://localhost:2375
-                    - name: DOCLET_OPTS
+                    - name: DOCKER_OPTS
                       value: ”$DOCKER_OPTS –insecure-registry=10.100.43.10:8083″
                 - name: dind-daemon 
                   image: docker:18.09.7-dind 
