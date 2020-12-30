9afe9ca984428365378d43d693b1210fd6d49e3d
mickey23alejo <mickey23alejo@gmail.com>
2020-12-30 18:30:43 -0500

    cambios

--- vars/qaLibrary.groovy
+++ vars/qaLibrary.groovy
@@ -58,33 +58,33 @@ def call(body) {
             }
         }
         stages {
-            stage('docker build') {
-                // steps {
-                //     container('buildah'){
-                //             //sh "podman build -f Dockerfile -t qa-'${config.name}'-image:v1.0.$BUILD_NUMBER ."
-                //             //sh "buildah bud -f Dockerfile -t qa-'${config.name}'-image:v1.0.$BUILD_NUMBER ."
-                //             sh "whoami"
-                //     }
-                // }
-                steps {
-                    container('docker'){
-                            // sh "docker version"
-                            // sh "whoami"
-                            // sh "docker ps"
-                            sh "docker build -f Dockerfile -t qa-'${config.name}'-image:v1.0.$BUILD_NUMBER ."
-                            //sh "docker login -u admin --password n2oxM-poryD-ew92Y-a2tFn 10.100.43.10:8083"
-                            //sh "docker login 10.128.3.113:8083"
-                            //sh "docker login -u admin --password $NEXUS_PASSWORD http://10.128.3.113:8083"
-                            //sh "SUCCESS_BUILD=`wget -qO- --user 'kubeadmin' --password 'n2oxM-poryD-ew92Y-a2tFn' --auth-no-challenge https://jenkins-jenkins.apps.ocp4mqa.grupodifare.com/job/EjemploMultibranch2/lastSuccessfulBuild/buildNumber`"
-                            //sh "echo SUCCESS_BUILD = $SUCCESS_BUILD - 1"
-                            //sh "docker tag qa-'${config.name}'-image:v1.0.$BUILD_NUMBER 10.128.3.113:8083/qa-'${config.name}'-image:v1.0.$BUILD_NUMBER"
-                    }
-                }
-            }
+            // stage('docker build') {
+            //     // steps {
+            //     //     container('buildah'){
+            //     //             //sh "podman build -f Dockerfile -t qa-'${config.name}'-image:v1.0.$BUILD_NUMBER ."
+            //     //             //sh "buildah bud -f Dockerfile -t qa-'${config.name}'-image:v1.0.$BUILD_NUMBER ."
+            //     //             sh "whoami"
+            //     //     }
+            //     // }
+            //     steps {
+            //         container('docker'){
+            //                 // sh "docker version"
+            //                 // sh "whoami"
+            //                 // sh "docker ps"
+            //                 sh "docker build -f Dockerfile -t qa-'${config.name}'-image:v1.0.$BUILD_NUMBER ."
+            //                 //sh "docker login -u admin --password n2oxM-poryD-ew92Y-a2tFn 10.100.43.10:8083"
+            //                 //sh "docker login 10.128.3.113:8083"
+            //                 //sh "docker login -u admin --password $NEXUS_PASSWORD http://10.128.3.113:8083"
+            //                 //sh "SUCCESS_BUILD=`wget -qO- --user 'kubeadmin' --password 'n2oxM-poryD-ew92Y-a2tFn' --auth-no-challenge https://jenkins-jenkins.apps.ocp4mqa.grupodifare.com/job/EjemploMultibranch2/lastSuccessfulBuild/buildNumber`"
+            //                 //sh "echo SUCCESS_BUILD = $SUCCESS_BUILD - 1"
+            //                 //sh "docker tag qa-'${config.name}'-image:v1.0.$BUILD_NUMBER 10.128.3.113:8083/qa-'${config.name}'-image:v1.0.$BUILD_NUMBER"
+            //         }
+            //     }
+            // }
             stage('wget'){
               steps{
                 container('wget'){
-                  sh "wget -qO- --user kubeadmin --password 'n2oxM-poryD-ew92Y-a2tFn' --auth-no-challenge https://jenkins-jenkins.apps.ocp4mqa.grupodifare.com/job/MicroservicesQA/job/Test.CargaMasiva2/lastSuccessfulBuild/buildNumber"
+                  sh "wget -qO- --user kubeadmin --password 'n2oxM-poryD-ew92Y-a2tFn' --auth-no-challenge https://jenkins-jenkins.apps.ocp4mqa.grupodifare.com/job/MicroservicesQA/job/Test.CargaMasiva2/job/%2500/lastSuccessfulBuild/buildNumber"
                 }
               }
             }
