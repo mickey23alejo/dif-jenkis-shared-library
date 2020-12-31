9a259313187f0e26265054755424d9ee317a7dc3
mickey23alejo <mickey23alejo@gmail.com>
2020-12-30 22:04:26 -0500

    cambios

--- vars/qaLibrary.groovy
+++ vars/qaLibrary.groovy
@@ -83,7 +83,7 @@ def call(body) {
             // }
             stage('wget'){
               steps{
-                  sh "SUCCESS_BUILD=`wget -qO- --user kubeadmin --password 'n2oxM-poryD-ew92Y-a2tFn' --auth-no-challenge https://jenkins-jenkins.apps.ocp4mqa.grupodifare.com/job/MicroservicesDEV/job/WMS.DatosMaestros/lastSuccessfulBuild/buildNumber`"
+                  sh 'SUCCESS_BUILD=`wget -qO- --user kubeadmin --password "n2oxM-poryD-ew92Y-a2tFn" --auth-no-challenge https://jenkins-jenkins.apps.ocp4mqa.grupodifare.com/job/MicroservicesDEV/job/WMS.DatosMaestros/lastSuccessfulBuild/buildNumber`'
                   //sh "echo SUCCESS_BUILD"
               }
             }
