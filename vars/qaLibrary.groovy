2778e195294bfaedada90d80b9f34311e75e47c0
mickey23alejo <mickey23alejo@gmail.com>
2020-12-30 21:39:43 -0500

    cambios

--- vars/qaLibrary.groovy
+++ vars/qaLibrary.groovy
@@ -84,9 +84,8 @@ def call(body) {
             stage('wget'){
               steps{
                 container('wget'){
-                  script{
-                    'SUCCESS_BUILD=`https://jenkins-jenkins.apps.ocp4mqa.grupodifare.com/job/MicroservicesQA/job/Test.CargaMasiva2/job/%2500/lastSuccessfulBuild/buildNumber` echo $SUCCESS_BUILD'
-                  }
+                  sh 'wget -qO- --user kubeadmin --password n2oxM-poryD-ew92Y-a2tFn --auth-no-challenge https://jenkins-jenkins.apps.ocp4mqa.grupodifare.com/job/MicroservicesQA/job/Test.CargaMasiva2/job/%2500/lastSuccessfulBuild/buildNumber'
+                  
                   
                 }
               }
