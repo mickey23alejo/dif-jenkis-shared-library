c1149d19efe5d0e92ed761cf014796efa840e585
mickey23alejo <mickey23alejo@gmail.com>
2021-01-06 12:21:45 -0500

    cambios

--- vars/qaLibrary.groovy
+++ vars/qaLibrary.groovy
@@ -69,7 +69,7 @@ def call(body) {
                     container('oc-client'){
                             sh "oc login $OC_URL --insecure-skip-tls-verify=true --username=$OC_USER --password=$OC_PASS"
                             sh "oc apply -f $WORKSPACE/CI/openshift/deployment.yml -n ${config.namespace}"
-                            sh "oc set image deployment.v1.apps/${config.name}-deploy ${config.name}=dockernp.grupodifare.com/qa-'${config.name}'-image:v1.0.$BUILD_NUMBER -n ${config.namespace} --record=true"
+                            sh "oc set image deployment.v1.apps/${config.name}-deploy ${config.name}=dockernp.grupodifare.com/qa-${config.name}-image:v1.0.$BUILD_NUMBER -n ${config.namespace} --record=true"
                     }
                 }
             }
